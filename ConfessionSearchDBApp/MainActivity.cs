36ba7f613f3aafaa3a0d073176ca10a9ed88d2ae
Brandon James Guerin <boomersooner12345@gmail.com>
2019-02-13 14:11:54 -0600

    Add field to WCF doc Table

--- ConfessionSearchDBApp/MainActivity.cs
+++ ConfessionSearchDBApp/MainActivity.cs
@@ -11,6 +11,7 @@ using Android.Content.PM;
 using Android;
 using Android.Util;
 using Android.Support.V4.App;
+using System;
 
 namespace ConfessionSearchDBApp
 {
@@ -28,7 +29,7 @@ namespace ConfessionSearchDBApp
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            TextView textView1 = FindViewById<TextView>(Resource.Id.textView1), text2 = FindViewById<TextView>(Resource.Id.textView2);
+          //  TextView textView1 = FindViewById<TextView>(Resource.Id.textView1), text2 = FindViewById<TextView>(Resource.Id.textView2);
             // if(ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.WriteExternalStorage != (int)Permission.Granted));
             if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) == (int)Permission.Granted)
             {
@@ -50,13 +51,16 @@ namespace ConfessionSearchDBApp
                 using (var conn = new SQLite.SQLiteConnection(dbPath))
                 {
                     var cmd = new SQLite.SQLiteCommand(conn);
+                    
                     cmd.CommandText = "select * from DocumentList";
-                    var r = cmd.ExecuteQuery<DocumentLists>();
+
+                    var r = cmd.ExecuteQuery<DocumentList>();
                     //foreach(string strings in r)
                     foreach (var item in r)
                     {
-                        textView1.Text += item.DocumentID + ". \r\n";
-                        text2.Text += item.Title + ". \r\n";
+
+                       // textView1.Text = Formatter(item.DocumentText);
+                       // text2.Text = Formatter(item.ChProofs);
                     }
 
 
@@ -73,15 +77,54 @@ namespace ConfessionSearchDBApp
                 // Camera permission is not granted. If necessary display rationale & request.
             }
         }
-        public class DocumentLists
+        public string Formatter(string stringField)
+        {
+            int x = 0;
+            string formatter = "";
+            string[] words;
+            words = stringField.Split('|');
+            for (int i = 0; i <= words.Length - 1; i++, x++)
+            {
+                formatter += words[i];
+                formatter += "\r\n\n";
+
+            }
+
+            stringField = formatter;
+            return stringField;
+        }
+        [Table("DocumentList")]
+        public class DocumentList
         {
             [PrimaryKey, AutoIncrement]
             public int DocumentID { get; set; }
             [Column("DocumentName")]
             public string Title { get; set; }
-           // public int ArtistId { get; set; }
+           
+            private int DocumentTypeID { get; set; }
+            // public int ArtistId { get; set; }
         }
-       
+       // [Table("Document")]
+       public class Document
+        {
+            [PrimaryKey,AutoIncrement]
+            public int DocDetailID { get; set; }
+            [Column("ChText")]
+            public string DocumentText { get; /*{ return Formatter(this.DocumentText); }*/ set; /*{ this.DocumentText = Formatter(value); }*/ }
+            
+
+            [Column("DocIndexNum")]
+            public int ChNumber { get; set; }
+[Column("ChProofs")]
+            public string ChProofs { get; /*{ return Formatter(this.ChProofs); }*/ set; } //{ this.ChProofs = Formatter(value); }}
+
+            [Column("ChMatches")]
+            public int Matches { get; set; }// { return this.Matches; } set { this.Matches = value; } }
+
+
+
+
+        }
 
     }
 }
